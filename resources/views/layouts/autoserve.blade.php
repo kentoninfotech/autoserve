34d58af6ece39839f352e9817a63de42e75cc5f4
anwokeoma <anwokeoma@yahoo.com>
2026-04-24 23:41:16 +0100

    A lot done on Invoice Page2

--- resources/views/layouts/autoserve.blade.php
+++ resources/views/layouts/autoserve.blade.php
@@ -23,8 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="https://www.autoprince.ng" target="_blank">
-                    <img src="{{ asset('images/logo.png') }}" alt="Auto Prince Technologies" style="height: 50px;">
+                <a class="navbar-brand" href="https://www.autoserve.com.ng" target="_blank">
+                    <img src="{{ asset('images/logo.png') }}" alt="AutoServe" style="height: 50px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
